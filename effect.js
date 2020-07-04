73cbf2524f12d89766b41d035a2154be66c5787a
Hanniel95 <61442648+Hanniel95@users.noreply.github.com>
2020-07-04 15:11:44 +0100

    Ajout du reload

--- effect.js
+++ effect.js
@@ -193,6 +193,32 @@ $('document').ready(function(){
 		msgLoop(0);
 		
 	});
+	
+	$("#restart").click(function(){
+		function msgLoop (i) {
+			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
+			i=i+1;
+			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
+			if(i==50){
+				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+					$('.cake').fadeIn('fast');
+				});
+
+			}
+			else{
+				msgLoop(i);
+			}			
+
+		});
+			// body...
+		}
+		
+		msgLoop(0);		
+	});
+	
+	$("#reload").click(function(){
+		location.reload();	
+	});
 });
 
 
