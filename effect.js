b9906215dc03bdd7488efe76e1f358eb53f55482
Hanniel95 <61442648+Hanniel95@users.noreply.github.com>
2020-07-04 15:43:58 +0100

    Update effect.js

--- effect.js
+++ effect.js
@@ -197,6 +197,7 @@ $('document').ready(function(){
 	
 	$("#restart").click(function(){
 		document.getElementById('final').style.display = 'none';
+		$('.cake').fadeOut('slow').delay(800);
 		function msgLoop (i) {
 			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
 			i=i+1;
@@ -204,6 +205,7 @@ $('document').ready(function(){
 			if(i==50){
 				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
 					$('.cake').fadeIn('fast');
+					document.getElementById('final').style.display = '';
 				});
 
 			}
